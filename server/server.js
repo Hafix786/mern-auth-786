b494984307792de985aa446307e1f669e75ec789
Hafix786 <ibnkhalid786@gmail.com>
2026-05-05 09:19:41 +0500

    Connected to frontend using render.com static website

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 4000
 
 connectDB()
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['mern-auth-786-ak.onrender.com']
 
 app.use(express.json())
 app.use(cookieParser())
